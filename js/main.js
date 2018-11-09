2714644ff5543cd92855ed38551763f57802d0f3
Caleb Trachte <cetrachte@hotmail.com>
2018-11-09 16:15:25 -0600

    add accesibility features to rendering restaurant html

--- js/main.js
+++ js/main.js
@@ -161,6 +161,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name; // add alt attribute for accessiblity
+  image.tabIndex = 0; // set tabindex for screen readers
   li.append(image);
 
   const name = document.createElement('h1');
@@ -169,10 +171,12 @@ createRestaurantHTML = (restaurant) => {
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.tabIndex = 0; // set tabindex for screen readers
   li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
+  address.tabIndex = 0; // set tabindex for screen readers
   li.append(address);
 
   const more = document.createElement('a');
