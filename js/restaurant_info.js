93119723fccc5ecec7cc6ae3989adfbdd4beb598
ctrachte <cetrachte@hotmail.com>
2018-11-12 14:08:52 -0600

    fix responsive styling, fix variable name

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiY3RyYWNodGUiLCJhIjoiY2pvYTlidHhjMGQxczNvbjBsMGNhaW95NCJ9.4ffU0qr3uXMd3kO8unKRTQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -90,7 +90,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  imag.alt = restaurant.name; // set alt attribute for accessiblity
+  image.alt = restaurant.name; // set alt attribute for accessiblity
   image.tabIndex = 0; // set the tab index for screen readers
 
 
